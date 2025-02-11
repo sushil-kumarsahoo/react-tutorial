ae66e09abdf4a0f6b4b60a343f806ae07e2d8cc0
sushil-kumarsahoo <suhilkmrsho@gmail.com>
2025-02-12 00:44:19 +0530

    counter project

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,10 @@
 import Sushil from "./sushil";
 function App() {
+  const username = 'sushil kumar sahoo'
   return (
     <>
       <Sushil />
-      <h1> hii</h1>
+      <h1> hii {username}</h1>
       <p>test para</p>
     </>
   );
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,43 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
-
 import App from './App.jsx'
+import React from 'react'
 
+function MyApp(){
+  return (
+    <div>
+      <h1>
+        Custom app!
+      </h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//       href:'https://google.com',
+//       target: '_blanlk'
+//   },
+//   children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+  <a href='https://google.com' target='_blank'> Visit Google</a>
+)
+
+
+const anotherUser = "sushil kumar Sahoo"
+
+const  reactElement = React.createElement(
+  'a',
+  {href:'https://google.com',target:'_blank' },
+  'click me to visit google',
+  anotherUser
+)
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+    
+  reactElement
+  
 )
